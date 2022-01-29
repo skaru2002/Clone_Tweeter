a62e73188b065399b7f01c3b0c16d6cf70f02f6a
meayang <skaru2002@naver.com>
2022-01-29 23:40:15 +0900

    socket으로 채팅구현

--- server/app.js
+++ server/app.js
@@ -6,6 +6,7 @@ import helmet from 'helmet';
 import tweetsRouter from './router/tweets.js';
 import authRouter from './router/auth.js';
 import { config } from './config.js';
+import { initSocket } from './connection/socket.js';
 
 const app = express();
 
@@ -25,4 +26,6 @@ app.use((error, req, res, next) => {
   console.error(error);
   res.sendStatus(500);
 });
-app.listen(config.host.port);
+
+const server = app.listen(config.host.port);
+initSocket(server);
